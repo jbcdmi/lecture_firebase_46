cd1af23132db61c64094538dbf3e15f0f7463be8
Jogani Bhautik <joganibhautik.cdmi@gmail.com>
2026-03-10 15:59:54 +0530

    add_viewdata_realtime_database

--- lib/Lecture466/ViewRealtimeDatabase.dart
+++ lib/Lecture466/ViewRealtimeDatabase.dart
@@ -17,8 +17,12 @@ class ViewRealtimeDatabase extends StatefulWidget {
 }
 
 class _ViewRealtimeDatabaseState extends State<ViewRealtimeDatabase> {
+  // String data = "loading...";
+  // using map
+  // List<Map<String, dynamic>> quotes = [];
+  // using model class
+  List<Quote> quotes = [];
 
-  String data = "loading...";
   @override
   void initState() {
     fetchData();
@@ -27,10 +31,10 @@ class _ViewRealtimeDatabaseState extends State<ViewRealtimeDatabase> {
 
   Future<void> fetchData() async {
     // DatabaseReference ref = FirebaseDatabase.instance.ref();
-    // DatabaseReference ref = FirebaseDatabase.instance.ref("quotes"); // way-1
+    DatabaseReference ref = FirebaseDatabase.instance.ref("quotes"); // way-1
     // DatabaseReference ref = FirebaseDatabase.instance.ref().child("quotes"); // way-2
 
-    DatabaseReference ref = FirebaseDatabase.instance.ref().child("quotes").child("0").child("author");
+    // DatabaseReference ref = FirebaseDatabase.instance.ref().child("quotes").child("0").child("author");
 
     // get data once
     // DataSnapshot snapshot = await ref.get();
@@ -43,18 +47,55 @@ class _ViewRealtimeDatabaseState extends State<ViewRealtimeDatabase> {
     // listen to changes
     ref.onValue.listen((event) {
       DataSnapshot snapshot = event.snapshot;
-      data = snapshot.value.toString();
-      setState(() {
-
-      });
-    },);
+      List ll = snapshot.value as List;
+      // way-1
+      // ll.forEach((element) {
+      //   Map<String, dynamic> map = Map.castFrom(element);
+      //   quotes.add(map);
+      //   print("::::: ${map["author"]}");
+      //   // print("::::: $element");
+      // });
+      // way-2
+      // quotes = ll.map((e) => Map<String, dynamic>.from(e)).toList();
+      // way-3
+      quotes = ll.map((e) => Quote.fromJson(Map<String, dynamic>.from(e)),).toList();
+      setState(() {});
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text(data, style: TextStyle(fontSize: 30),));
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: quotes.length,
+        itemBuilder: (context, index) {
+          // using map
+          // Map map = quotes[index];
+          // return ListTile(title: Text(map["author"]));
+
+          // using model class
+          Quote quote = quotes[index];
+          return ListTile(title: Text(quote.quote),);
+        },
+      ),
+    );
   }
 }
+
+// model class
+class Quote {
+  String author;
+  String quote;
+  int id;
+
+  Quote(this.id, this.author, this.quote);
+
+  factory Quote.fromJson(Map<String, dynamic> json) {
+    return Quote(json["id"], json["author"], json["quote"]);
+  }
+}
+
+// map to class object
 
 /*
 *   Realtime Database
